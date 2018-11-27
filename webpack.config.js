3ab7978eea170bdeec79cf021dffd403ad242f3f
raylyanway <raylyanway@gmail.com>
2018-11-28 01:48:15 +0300

    template

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
