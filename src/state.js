b5405f61ba8f0a2d0b7495f52ac6ab4804fca4c7
iLya Raylyan <raylyanway@gmail.com>
2018-12-03 14:50:09 +0300

    Refactor

--- src/state.js
+++ src/state.js
@@ -1,17 +1,17 @@
-const visualState = {
+const visualState = () => ({
   addingProcess: {
     valid: true,
     submitDisabled: true,
     inputDisabled: false,
     help: '',
   },
-};
+});
 
-const structuralState = {
+const structuralState = () => ({
   newFeed: '',
   newArticleList: [],
   feeds: new Map(),
   articles: new Map(),
-};
+});
 
 export { visualState, structuralState };
