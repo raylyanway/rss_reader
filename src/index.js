23d99bdc8e2845efb03f6d335fb82a16193c1043
raylyanway <raylyanway@gmail.com>
2018-12-05 23:25:00 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import 'bootstrap';
 import { initVisualState, initStructuralState } from './state';
 import initWatchers from './watchers';
 import { app, reloadFeeds } from './app';
+import { renderForm } from './renderers';
 
 const visualState = initVisualState();
 const structuralState = initStructuralState();
 
+renderForm(visualState);
 initWatchers(visualState, structuralState);
 app(visualState, structuralState);
 reloadFeeds(visualState, structuralState);
